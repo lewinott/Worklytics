383e2c6a6182b856245c35a40d712ea10adf8c0d
Pinott Natalia (SO/JVL-ETS) <pin3jvl@bosch.com>
2025-03-07 11:25:30 -0300

    :sparkles: feat: Filtro por data

--- frontend/src/components/produtivity/filter-data/main.tsx
+++ frontend/src/components/produtivity/filter-data/main.tsx
@@ -1,14 +1,41 @@
-import React, { useState, } from "react";
+import React, { useState, useRef  } from "react";
 import * as S from "./styles"
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css"; 
+import "react-date-range/dist/theme/default.css"; 
 
 
 const FilterDate = () => {
+const [range, setRange] = useState([
+    { startDate: new Date(), endDate: new Date(), key: "selection" },
+  ]);
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
 
+const toggleCalendar = () => setOpen(!open);
   
     return (
-      <div>
- 
+      <S.Wrapper>
+      <input
+        type="text"
+        readOnly
+        onClick={toggleCalendar}
+        value={`${range[0].startDate.toLocaleDateString()} - ${range[0].endDate.toLocaleDateString()}`}
+      />
+      {open && (
+        <div className="calendar">
+          <DateRange
+            ranges={range}
+            onChange={(item) => setRange([item.selection])}
+            showPreview={false}
+            showDateDisplay={false}
+            moveRangeOnFirstSelection={false}
+          /> 
       </div>
+)}
+    </S.Wrapper>
     );
   };
+
+
 export default FilterDate
--- frontend/src/components/produtivity/filter-data/styles.ts
+++ frontend/src/components/produtivity/filter-data/styles.ts
@@ -1,3 +1,38 @@
 import styled from "styled-components";
 import { colors } from "../../../styles/theme";
 
+export const Wrapper = styled.div`
+  position: relative;
+  margin-top: 10px;
+
+  input {
+    width: 292px;
+    height: 48px;
+    padding: 4px 16px 0px 16px;
+    background-color: ${colors.boschgray90};
+    border-bottom: 1px solid ${colors.boschblack};
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    font-size: 16px;
+    outline: none;
+    cursor: pointer;
+    
+    &:hover{
+        background-color: ${colors.boschgray80};
+    }
+  }
+
+  .calendar {
+    position: absolute;
+    left: 0;
+    z-index: 1;
+    background: white;
+    border-radius: 8px;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    transform: scale(0.53); 
+    transform-origin: top left;
+  }
+`;
+
+
